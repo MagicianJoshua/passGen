b1fbd36e8f17412c4f725319cdcd7bd32574ba44
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2023-08-06 02:39:19 -0700

    checked to see if the input for charChoice was a number and if it isnt we ask the user to insert a number instead

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -5,15 +5,12 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 function generatePassword() {
   var allChoice = [];
@@ -26,7 +23,7 @@ function generatePassword() {
   var lettersUpper = Array.from(lettersUpper);
   // console.log(lettersUpper);
 
-  var specialChar = " !\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~";
+  var specialChar = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
   var specialChar = Array.from(specialChar);
   // console.log(specialChar);
 
@@ -34,40 +31,61 @@ function generatePassword() {
   var numeros = Array.from(numeros);
   // console.log(numeros);
 
-  let charChoice = window.prompt("how many characters would you like to have? ");
-  let num = parseInt(charChoice);
-  
-  let specialCharChoice = window.confirm("Would you like to include special chracters? \nCLick ok for yes and cancel for no.")
+  const charChoice = window.prompt(
+    "how many characters would you like to have? "
+  );
+  const num = parseInt(charChoice);
+
+  if (isNaN(num) === true){
+    window.prompt("Please enter a number.")
+    console.log(isNaN("Isnan " +num));
+    generatePassword();
+    return;
+  }else {
+    console.log(num);
+    console.log(isNaN("Isnan " +num));
+    
+  }
+
+  const specialCharChoice = window.confirm(
+    "Would you like to include special chracters? \nCLick ok for yes and cancel for no."
+  );
+
   if (specialCharChoice === true) {
     allChoice.push(specialChar);
     console.log(allChoice);
   }
-  
-  
-  let upperChoice = window.confirm("Would you like to include uppercase letters in your password \nClick ok for yes and cancel for no") ;
+
+  const upperChoice = window.confirm(
+    "Would you like to include uppercase letters in your password \nClick ok for yes and cancel for no"
+  );
+
   if (upperChoice === true) {
     allChoice.push(lettersUpper);
     console.log(allChoice);
   }
 
-  let numeroChoice =  window.confirm("Would you like to include numbers in your password \nClick ok for yes and cancel for no") ;
+  const numeroChoice = window.confirm(
+    "Would you like to include numbers in your password \nClick ok for yes and cancel for no"
+  );
+
   if (numeroChoice === true) {
     allChoice.push(numeros);
     console.log(allChoice);
   }
 
-  let lowerChoice = window.confirm("Would you like to include lower case letters in your password \nClick ok for yes and cancel for no") ;
+  const lowerChoice = window.confirm(
+    "Would you like to include lower case letters in your password \nClick ok for yes and cancel for no"
+  );
+
   if (lowerChoice === true) {
     allChoice.push(lettersLower);
     console.log(allChoice);
   }
-  
 
-
- 
-  // if (typeof num  == "number"){ 
+  // if (typeof num  == "number"){
   //   window.alert("Generating password");
-  // } 
+  // }
   // else {
   //   window.alert("Please enter a number.");
   // }
@@ -75,10 +93,8 @@ function generatePassword() {
   // for (i = 0; i < num ; i ++) {
   //   let rand = Math.floor(Math.random() * letters.length);
   //   pass.push(letters[rand])
-  // } 
+  // }
   // var numPass = pass.toString();
   // numPass = numPass.replaceAll("," ,"");
   // window.alert(numPass);
-
 }
-
