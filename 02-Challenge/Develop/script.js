3633bdba5ee7ad827400e10485727b0805049aa1
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2023-08-06 01:28:29 -0700

    inital commit

---
+++ 02-Challenge/Develop/script.js
@@ -0,0 +1,47 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+  const lettersLower = "abcdefghijklmnopqrstuvwxyz"
+  const lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const specialChar = " !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
+  const numeros = "123456789"
+ 
+  let charChoice = window.prompt("how many characters would you like to have? ");
+  let num = parseInt(charChoice);
+
+  let specialCharChoice = window.confirm("Would you like to include special chracters? \nCLick ok for yes and cancel for no.")
+  window.alert(specialCharChoice);
+
+ 
+  // if (typeof num  == "number"){ 
+  //   window.alert("Generating password");
+  // } 
+  // else {
+  //   window.alert("Please enter a number.");
+  // }
+
+  // for (i = 0; i < num ; i ++) {
+  //   let rand = Math.floor(Math.random() * letters.length);
+  //   pass.push(letters[rand])
+  // } 
+  // var numPass = pass.toString();
+  // numPass = numPass.replaceAll("," ,"");
+  // window.alert(numPass);
+
+}
+
